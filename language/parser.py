51bd02b34f7a4bbdf0cb4fec92a1144ac765e55d
SB680 <76981086+SB680@users.noreply.github.com>
2021-01-06 13:00:19 -0500

    Update parser.py

--- language/parser.py
+++ language/parser.py
@@ -23,8 +23,7 @@ AP -> A | A AP
 NP -> N | D NP | AP NP | N PP 
 PP -> P NP
 VP -> V | V NP | V NP PP | V PP 
-""" 
-#to VP I added V PP such as 'sat in the home'; else 'holmes sat in the home' cannot be parsed 
+"""  
 
 grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
 parser = nltk.ChartParser(grammar)
@@ -32,10 +31,10 @@ parser = nltk.ChartParser(grammar)
 
 def main(filename):
 
-    # If filename specified, read sentence from file
-    #with open(f"sentences", encoding="utf-8") as f:
-    if len(filename) < 6: 
-        with open(f"sentences/{filename}") as f: #you CAN'T OPEN A DIRECTORY BUT MUST SPECIFY A FILE WITHIN IT 
+    #If filename specified, read sentence from file
+    #otherwise, read the sentence as the filename 
+    if len(filename) < 6: #all the filenames in sentences have a smaller length 
+        with open(f"sentences/{filename}") as f:  
             s = f.read().strip('#,!@$%^&*()~`.?,<>{}[]":;')
 
         s = s.translate(str.maketrans('', '', string.punctuation))
@@ -64,29 +63,7 @@ def main(filename):
         for np in np_chunk(tree):
             print(" ".join(np.flatten()))
 
-
-def hasnp(phrase):
-    trees = list(parser.parse(phrase))
-    for tree in trees: 
-        tree.pretty_print()
-        tree = nltk.tree.ParentedTree.convert(tree)
-        parented_tree = nltk.tree.ParentedTree.convert(tree)  
-        for subtree in parented_tree.subtrees(lambda t: t.label() == 'NP'):
-            for tree in subtree.subtrees(lambda t: t.label() == 'NP'):
-                if not tree == subtree: 
-                    return False
-    return True
-
-def hasnp2(trees): 
-    for tree in trees: 
-        #tree.pretty_print()
-        tree = nltk.tree.ParentedTree.convert(tree)
-        parented_tree = nltk.tree.ParentedTree.convert(tree)  
-        for subtree in parented_tree.subtrees(lambda t: t.label() == 'NP'):
-            for tree in subtree.subtrees(lambda t: t.label() == 'NP'):
-                if not tree == subtree: 
-                    return False
-    return True 
+ 
             
 def preprocess(sentence):
     """
@@ -94,12 +71,17 @@ def preprocess(sentence):
     Pre-process sentence by converting all characters to lowercase
     and removing any word that does not contain at least one alphabetic
     character.
-    """
-    #print("SENTENCE: ",sentence, type(sentence)); sentence = sentence.strip('#,!@$%^&*()~`.?,<>{}[]":;')
-    #print("SENTENCE: ",sentence, type(sentence))
+    """ 
     sentence = (nltk.word_tokenize(sentence.lower()))   #a weakness: this won't strip a single apostrophe(try it to see why) 
-    #print("SENTENCE: ",sentence) 
     return sentence 
+
+def hasnounphrase(subtree):
+    for subt in subtree.subtrees():
+        if subt == subtree:
+            continue
+        elif subt.label() == 'NP':
+            return True
+    return False
 
 def np_chunk(tree):
     """
@@ -108,16 +90,12 @@ def np_chunk(tree):
     whose label is "NP" that does not itself contain any other
     noun phrases as subtrees.
     """
-    np_chunks = []
-    parented_tree = nltk.tree.ParentedTree.convert(tree)
+    np_chunks = [] 
     
     for subtree in parented_tree.subtrees(lambda t: t.label() == 'N'):
-        np_chunks.append(subtree)
-
+        if not hasnounphrase(subtree):
+            np_chunks.append(subtree)
     return np_chunks
-
-#print (hasnp(['armchair'])) 
+ 
 if  __name__ == "__main__":
-    main('the armchair in the home smiled') #must write 1.txt and NOT 1 
-
-#in the home 
+    main('the armchair in the home smiled') 
